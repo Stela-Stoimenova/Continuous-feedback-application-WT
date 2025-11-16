da989527026b0d3a38ed684cfcb21c1d8dad4833
TurcanuTeodor <turcanuteodor23@stud.ase.ro>
2025-11-16 14:53:55 +0200

    Added backend structure

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+// A common pattern in Express is: routes → controllers → services → models.
+// Type structure                 
+// backend/                     
+// ├── controllers/ 
+// │   └── ?.js
+// ├── models/
+// │   └── ?.js
+// ├── routes/
+// │   └── ?.js
+// ├── services/
+// │   └── ?.js
+// ├── app.js
+// └── package.json
